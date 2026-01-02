c4dd52748e7ba728d15d8006ece268dd755b29b5
kenbinner <kengo@binner.org>
2026-01-02 12:17:56 +0000

    Initial commit

---
+++ src/main/java/com/kenbinner/coffeeblog_backend/CoffeeblogBackendApplication.java
@@ -0,0 +1,13 @@
+package com.kenbinner.coffeeblog_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeblogBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeblogBackendApplication.class, args);
+	}
+
+}
